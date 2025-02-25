61b8e330a82231210ae50f9cbe1f887f6713d4e4
SamenRyan <163199745+SamenRyan@users.noreply.github.com>
2025-02-25 23:52:35 +0100

    Add files via upload

---
+++ build/app/generated/source/buildConfig/release/com/example/otantik_quotes/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.otantik_quotes;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.otantik_quotes";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
